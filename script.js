d68cf20f268554e4f1439d26f298e38186ebc692
MichaelFrancoo2006 <michaelfrancoo1827@gmail.com>
2025-04-09 22:16:48 +0300

    Get the list of recipes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Function to filter recipes based on search input
 function filterRecipes() {
     const searchInput = document.getElementById("search").value.toLowerCase();
+    const recipeList = document.getElementById("recipe-list");
 }
